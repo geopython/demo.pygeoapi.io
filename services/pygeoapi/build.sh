093dd7647987b64cd2fa6db497d800b5578d6584
Just van den Broecke <just@justobjects.nl>
2019-05-17 13:14:12 +0200

    pygeoapi service Docker fixes

--- services/pygeoapi/build.sh
+++ services/pygeoapi/build.sh
@@ -2,4 +2,4 @@
 #
 # Build apache2 Docker image with options and docs
 
-docker build -t pygeoapi/pygeoapi:latest .
+docker build -t geopython/pygeoapi:latest .
--- services/pygeoapi/entrypoint.sh
+++ services/pygeoapi/entrypoint.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
+# Runs the pygeoapi app with gunicorn
+
+echo "START /entrypoint.sh"
+
 export PYGEOAPI_HOME=/pygeoapi
 export PYGEOAPI_CONFIG="${PYGEOAPI_HOME}/local.config.yml"
 export PYGEOAPI_OPENAPI="${PYGEOAPI_HOME}/local.openapi.yml"
 
 set +e
-echo "Trying to generate openapi.yml"
 cd ${PYGEOAPI_HOME}
 
+echo "Trying to generate openapi.yml"
 pygeoapi generate-openapi-document -c ${PYGEOAPI_CONFIG} > ${PYGEOAPI_OPENAPI}
 
 if [ $? -ne 0 ] ; then
-     echo "openapi.yml could not be generated ERROR, but carry on"
- else
- 	 echo "openapi.yml generated continue to pygeoapi"
- fi
+     echo "openapi.yml could not be generated ERROR"
+     exit -1
+fi
 
-pygeoapi serve
+echo "openapi.yml generated continue to pygeoapi"
+
+# SCRIPT_NAME should not have value '/'
+[ "${SCRIPT_NAME}" = '/' ] && export SCRIPT_NAME="" && echo "make SCRIPT_NAME empty from /"
+
+echo "Running pygeoapi  WSGI on ${CONTAINER_HOST}:${CONTAINER_PORT} with ${WSGI_WORKERS} workers and SCRIPT_NAME=${SCRIPT_NAME}"
+gunicorn --workers ${WSGI_WORKERS} \
+		--worker-class=${WSGI_WORKER_CLASS} \
+		--timeout ${WSGI_WORKER_TIMEOUT} \
+		--name=${CONTAINER_NAME} \
+		--bind ${CONTAINER_HOST}:${CONTAINER_PORT} \
+		pygeoapi.flask_app:APP
+
+echo "END /entrypoint.sh"

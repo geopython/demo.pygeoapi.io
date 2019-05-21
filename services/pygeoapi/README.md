062d7876009e96aef6e8e0969bba264c660ac8aa
Just van den Broecke <just@justobjects.nl>
2019-05-21 12:42:27 +0200

    geopython/pygeoapi#112 Dockerfile using Deb Buster Slim and run tests in entrypoint

--- services/pygeoapi/README.md
+++ services/pygeoapi/README.md
@@ -1,4 +1,11 @@
 # pygeoapi service
 
-Runs latest GitHub master branch of pygeoapi using Docker.
+Runs latest GitHub master branch of `pygeoapi` using Docker.
 
+## Running Tests
+
+By default this Image will start `pygeoapi` using `gunicorn`, but you can also run tests:
+
+```
+	docker run -it geopython/pygeoapi test
+```
--- services/pygeoapi/entrypoint.sh
+++ services/pygeoapi/entrypoint.sh
@@ -4,32 +4,69 @@
 
 echo "START /entrypoint.sh"
 
+set +e
+
 export PYGEOAPI_HOME=/pygeoapi
 export PYGEOAPI_CONFIG="${PYGEOAPI_HOME}/local.config.yml"
 export PYGEOAPI_OPENAPI="${PYGEOAPI_HOME}/local.openapi.yml"
 
-set +e
+# What to invoke: default is to run gunicorn server
+entry_cmd=${1:-run}
+
+# Shorthand
+function error() {
+	echo "ERROR: $@"
+	exit -1
+}
+
+# Workdir
 cd ${PYGEOAPI_HOME}
 
 echo "Trying to generate openapi.yml"
 pygeoapi generate-openapi-document -c ${PYGEOAPI_CONFIG} > ${PYGEOAPI_OPENAPI}
 
-if [ $? -ne 0 ] ; then
-     echo "openapi.yml could not be generated ERROR"
-     exit -1
-fi
+[[ $? -ne 0 ]] && error "openapi.yml could not be generated ERROR"
 
 echo "openapi.yml generated continue to pygeoapi"
 
-# SCRIPT_NAME should not have value '/'
-[ "${SCRIPT_NAME}" = '/' ] && export SCRIPT_NAME="" && echo "make SCRIPT_NAME empty from /"
+case ${entry_cmd} in
+	test)
+	  for test_py in $(ls tests/test_*.py)
+	  do
+	    # Skip tests requireing backend server or libs installed
+	    case ${test_py} in
+	        tests/test_elasticsearch__provider.py)
+	        ;&
+	        tests/test_gpkg_provider.py)
+	        ;&
+	        tests/test_postgresql_provider.py)
+	        ;&
+	        tests/test_sqlite_provider.py)
+	        	echo "skip: ${test_py}"
+	        ;;
+	        *)
+	        	python3 -m pytest ${test_py}
+	         ;;
+	    esac
+	  done
+	  ;;
 
-echo "Running pygeoapi  WSGI on ${CONTAINER_HOST}:${CONTAINER_PORT} with ${WSGI_WORKERS} workers and SCRIPT_NAME=${SCRIPT_NAME}"
-gunicorn --workers ${WSGI_WORKERS} \
-		--worker-class=${WSGI_WORKER_CLASS} \
-		--timeout ${WSGI_WORKER_TIMEOUT} \
-		--name=${CONTAINER_NAME} \
-		--bind ${CONTAINER_HOST}:${CONTAINER_PORT} \
-		pygeoapi.flask_app:APP
+	run)
+		# SCRIPT_NAME should not have value '/'
+		[[ "${SCRIPT_NAME}" = '/' ]] && export SCRIPT_NAME="" && echo "make SCRIPT_NAME empty from /"
+
+		echo "Running pygeoapi  WSGI on ${CONTAINER_HOST}:${CONTAINER_PORT} with ${WSGI_WORKERS} workers and SCRIPT_NAME=${SCRIPT_NAME}"
+		gunicorn --workers ${WSGI_WORKERS} \
+				--worker-class=${WSGI_WORKER_CLASS} \
+				--timeout ${WSGI_WORKER_TIMEOUT} \
+				--name=${CONTAINER_NAME} \
+				--bind ${CONTAINER_HOST}:${CONTAINER_PORT} \
+				pygeoapi.flask_app:APP
+	  ;;
+	*)
+	  error "unknown command arg: userun or test"
+	  ;;
+esac
+
 
 echo "END /entrypoint.sh"

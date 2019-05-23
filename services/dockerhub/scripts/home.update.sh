5ea39e20b7b437c6d3a21bdb7243f395711b711d
Just van den Broecke <just@justobjects.nl>
2019-05-23 16:18:57 +0200

    geopython/pygeoapi#112 refine DockerHub webhook for demo home app, rename docker images

--- services/dockerhub/scripts/home.update.sh
+++ services/dockerhub/scripts/home.update.sh
@@ -2,6 +2,4 @@
 
 echo "Updating pygeoapi..."
 
-/src/scripts/docker-clean.sh
-
 cd /srv/home && ./stop.sh && docker pull geopython/demo.pygeoapi.io:latest && ./start.sh
--- services/dockerhub/scripts/pygeoapi.update.sh
+++ services/dockerhub/scripts/pygeoapi.update.sh
@@ -2,6 +2,4 @@
 
 echo "Updating pygeoapi..."
 
-/src/scripts/docker-clean.sh
-
 cd /srv/pygeoapi && ./stop.sh && docker pull geopython/pygeoapi:latest && ./build.sh && ./start.sh

db722d52db7099cde4b096216b654c7c4d82ff5f
Just van den Broecke <just@justobjects.nl>
2020-01-13 18:37:50 +0100

    #7 - geopython/pygeoapi#325 trying to get ES running - now ok, remains pygeoapi container

--- services/pygeoapi_cite/elasticsearch/docker-entrypoint.sh
+++ services/pygeoapi_cite/elasticsearch/docker-entrypoint.sh
@@ -32,8 +32,7 @@
 
 # wait for Elasticsearch to start, then run the setup script to
 # create and configure the index.
-# exec /usr/share/elasticsearch/bin/wait-for-it.sh localhost:9200 -- /add_data.sh &
-# exec $@
-exec /usr/share/elasticsearch/bin/elasticsearch
 
+exec /usr/share/elasticsearch/bin/wait-for-it.sh localhost:9200 -- /add_data.sh &
+exec /usr/local/bin/docker-entrypoint.sh
 # /wait-for-elasticsearch.sh http://elasticsearch:9200 /entrypoint.sh || echo "Elasticsearch failed: $?, exit" && exit 1

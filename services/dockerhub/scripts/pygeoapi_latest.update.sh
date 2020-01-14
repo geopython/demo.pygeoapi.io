5887dd607b66a1c5b2eac01fc66a7a03217380ca
Just van den Broecke <just@justobjects.nl>
2020-01-14 11:51:01 +0100

    #7 - geopython/pygeoapi#325 fix dockerhub webhook deploy

--- services/dockerhub/scripts/pygeoapi_latest.update.sh
+++ services/dockerhub/scripts/pygeoapi_latest.update.sh
@@ -2,11 +2,13 @@
 
 echo "Updating pygeoapi_master..."
 
-cd /srv/pygeoapi_master && ./stop.sh && docker pull geopython/pygeoapi:latest && ./build.sh && docker-compose up -d
+docker pull geopython/pygeoapi:latest
+
+cd /srv/pygeoapi_master && ./stop.sh && ./build.sh && docker-compose up -d
 
 echo "Updating pygeoapi_cite..."
 
-cd /srv/pygeoapi_cite && ./stop.sh && docker pull geopython/pygeoapi:latest && ./build.sh && docker-compose up -d
+cd /srv/pygeoapi_cite && ./stop.sh && ./build.sh && docker-compose up -d
 
 echo "Cleaning up..."
 

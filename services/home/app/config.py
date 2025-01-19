f815cb645bc65b970d3673eac30de0f5e95f00db
Tom Kralidis <tomkralidis@gmail.com>
2025-01-19 10:52:41 -0500

    add api-catalog support (#60)
    * add api-catalog support
    
    * fix anchor link
    
    * add link to homepage
    
    * add link relation

--- services/home/app/config.py
+++ services/home/app/config.py
@@ -1,7 +1,16 @@
 import logging
 import os
 
+base_url = 'https://demo.pygeoapi.io'
+
 # local config, change on server for real config
 config = {
     'loglevel': int(os.getenv('ADMIN_LOG_LEVEL', logging.DEBUG))
 }
+
+services = [
+    {'href': '/master', 'title': "pygeoapi - latest GitHub 'master' version"},
+    {'href': '/stable', 'title': 'pygeoapi - latest stable version'},
+    {'href': '/cite', 'title': "pygeoapi - CITE endpoint - latest GitHub 'master' version"},  # noqa
+    {'href': '/covid-19', 'title': "pygeoapi - COVID-19 endpoint - latest GitHub 'master' version"}  # noqa
+]

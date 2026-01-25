1cefdc782866f21d25328efed9b793aa8fa79265
Just van den Broecke <just@justobjects.nl>
2026-01-25 14:37:40 +0100

    #74 use cron on host for processes cleanup - fix tmp filenames

--- services/pygeoapi_master/process_cleanup.sh
+++ services/pygeoapi_master/process_cleanup.sh
@@ -2,4 +2,4 @@
 # Cleanup tempfiles in Container created by pygeoapi Processes.
 # Author: Just van den Broecke - 2026
 
-docker exec pygeoapi_master sh -c 'rm -f /tmp/pygeoapi-process-manager.* hello-world*'
+docker exec pygeoapi_master sh -c 'rm -f /tmp/pygeoapi-process-manager.* /tmp/hello-world-*'

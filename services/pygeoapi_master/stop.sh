3bc4012a32e6ae89aa09e97d2b27cc195f63c5a1
Just van den Broecke <just@justobjects.nl>
2026-01-25 14:29:18 +0100

    #74 use cron on host for processes cleanup - remove comment

--- services/pygeoapi_master/stop.sh
+++ services/pygeoapi_master/stop.sh
@@ -5,4 +5,4 @@ source ../env.sh
 # Only on prod servers, remove process cleanup job
 [[ ${SITE_DOMAIN} == "NONE" ]] || sudo /bin/rm /etc/cron.d/pygeoapi_process_cleanup > /dev/null 2>&1
 
-# docker compose down --remove-orphans
+docker compose down --remove-orphans
